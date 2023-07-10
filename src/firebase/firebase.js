b4a4abd7aaff0f4470279baeb92de9b98f59408a
unknown <dm7659746@gmail.com>
2023-07-10 13:20:51 -0500

    descargar excel

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,16 +1,17 @@
+require('dotenv').config();
 const firebase = require('firebase/compat/app');
 require('firebase/compat/auth');
 require('firebase/compat/firestore');
 require('firebase/compat/storage');
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBIy6XXzW_T9-Q3EvtP4sQ9KDHShzmZwCQ",
-    authDomain: "botpress-firestore.firebaseapp.com",
-    projectId: "botpress-firestore",
-    storageBucket: "botpress-firestore.appspot.com",
-    messagingSenderId: "14169208807",
-    appId: "1:14169208807:web:4cbcfe0a57fa9878bd2f42",
-    measurementId: "G-1W9PL8MMMJ"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId:process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
